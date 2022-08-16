3927049463e9a18ef7545c773206ff84118d19fa
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-16 13:04:36 +0530

    changes

--- S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
@@ -8,9 +8,22 @@ public class App
     {
        ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
        
-       Employee emp = (Employee) springContainer.getBean("emp"); 
-       System.out.println(emp);
-       Hospital hospital = (hospital) springContainer.getBean("hospital"); 
-       System.out.println(hospital);
+//       Employee emp = (Employee) springContainer.getBean("emp"); 
+//       System.out.println(emp);
+       
+       
+//       Employee em1 = new Employee();
+//       em1.setId(10);
+//       em1.setName("x");
+
+//       Hospital hospital = (Hospital) springContainer.getBean("hospital");
+//       System.out.println(hospital);
+       
+       
+//       CarDealer dealer = (CarDealer) springContainer.getBean("cardealer");
+//       System.out.println(dealer);
+       
+       Customer customer = (Customer) springContainer.getBean("customer");
+       System.out.println(customer);
     }
 }
