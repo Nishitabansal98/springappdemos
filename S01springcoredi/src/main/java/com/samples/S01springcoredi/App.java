c950a8281bb5c4a0648162300f8ede6acddc4a0f
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-17 10:22:02 +0530

    changes

--- S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
+++ S01springcoredi/src/main/java/com/samples/S01springcoredi/App.java
@@ -1,12 +1,15 @@
 package com.samples.S01springcoredi;
 
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-       ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
+       
+    	// create the spring container
+//    	ClassPathXmlApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
        
 //       Employee emp = (Employee) springContainer.getBean("emp"); 
 //       System.out.println(emp);
@@ -23,14 +26,19 @@ public class App
 //       CarDealer dealer = (CarDealer) springContainer.getBean("cardealer");
 //       System.out.println(dealer);
        
- //      Customer customer = (Customer) springContainer.getBean("customer");
- //      System.out.println(customer);
-       ShoppingCart sc = (ShoppingCart) springContainer.getBean("shoppingcart");
-       System.out.println(sc);
-    
-    
+//       Customer customer = (Customer) springContainer.getBean("customer");
+//       System.out.println(customer);
+    	
+//    	CountriesAndLanguages countries = (CountriesAndLanguages) springContainer.getBean("countries");
+//    	System.out.println(countries);
+    	
+//    	Student student = (Student) springContainer.getBean("student");
+//    	System.out.println(student);
+    	
+    	AbstractApplicationContext springContainer = new ClassPathXmlApplicationContext("com/samples/S01springcoredi/SpringConfig.xml");
+    	Patient patient = (Patient) springContainer.getBean("patient");
+    	System.out.println(patient);
+    	springContainer.registerShutdownHook();
     }
-    
-    
-    
 }
+    
