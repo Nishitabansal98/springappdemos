22b86bdd37dfc884d1fd66315f9a15d7705b8292
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-21 18:11:43 +0530

    add

--- SpringCoreProject/src/main/java/com/SpringProject/SpringCoreProject/App.java
+++ SpringCoreProject/src/main/java/com/SpringProject/SpringCoreProject/App.java
@@ -12,7 +12,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
-        Student student1=(Student) context.getBean("student1")
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/SpringProject/SpringCoreProject/Config.xml");
+        Student student1=(Student) context.getBean("student1");
+        System.out.println(student1);
     }
 }
