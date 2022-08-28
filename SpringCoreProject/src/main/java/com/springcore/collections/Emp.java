b935861559580bdc32d789f1f45b9c0e56d0e8f8
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-08-28 23:14:04 +0530

    changes

---
+++ SpringCoreProject/src/main/java/com/springcore/collections/Emp.java
@@ -0,0 +1,12 @@
+package com.springcore.collections;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Emp {
+	private List<String> phones;
+	private Set<String> addresses;
+	private Map<String,String> courses;
+
+}
