3fa303f3ba7e43ced8978c54f5513f52daa92d9a
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-02 00:42:47 +0530

    change

--- SpringCoreProject/src/main/java/com/springcore/auto/wire/Test.java
+++ SpringCoreProject/src/main/java/com/springcore/auto/wire/Test.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
 	public static void main(String[] args) {
-		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/auto/wire/autoconfig.xml");
+	ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/auto/wire/autoconfig.xml");
 	Emp emp1 = context.getBean("emp1" ,Emp.class);
 	System.out.println(emp1);
 	}
