c00259077a300d486d2b6dd6ef3a9765e78a0a98
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-02 00:23:46 +0530

    change

--- SpringCoreProject/src/main/java/com/springcore/auto/wire/Test.java
+++ SpringCoreProject/src/main/java/com/springcore/auto/wire/Test.java
@@ -1,14 +1,13 @@
 package com.springcore.auto.wire;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
-
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/auto/wire/autoconfig.xml");
+	Emp emp1 = context.getBean("emp1" ,Emp.class);
+	System.out.println(emp1);
 	}
 
 }
