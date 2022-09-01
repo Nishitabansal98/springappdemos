47d2f560f57dac7019bc26a8b92f1c9f3ee8f840
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-02 02:23:14 +0530

    change

--- SpringCoreProject/src/main/java/com/springcore/stereotype/Test.java
+++ SpringCoreProject/src/main/java/com/springcore/stereotype/Test.java
@@ -1,9 +1,13 @@
 package com.springcore.stereotype;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
 
-	public Test() {
-		// TODO Auto-generated constructor stub
+	public static void main(String[] args) {
+	ApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/stereoconfig.xml");
+		Student student = context.getBean("student",Student.class);
+		System.out.println(student);
 	}
-
 }
